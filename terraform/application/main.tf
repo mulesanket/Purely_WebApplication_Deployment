2a66095829210708b87e0d2c9bf85fedc119b0aa
Sanket Arun Mule <sanketmu@cybage.com>
2025-10-07 16:57:55 +0530

    project structure refinement

--- terraform/application/main.tf
+++ terraform/application/main.tf
@@ -1,10 +1,6 @@
-########################################
-# Root Environment: dev
-########################################
-
 # --- VPC Module ---
 module "vpc" {
-  source               = "../../modules/vpc"
+  source               = "../modules/vpc"
   cluster_name         = var.cluster_name
   name_prefix          = var.name_prefix
   vpc_cidr             = var.vpc_cidr
@@ -15,7 +11,7 @@ module "vpc" {
 
 # --- Bastion Host Module ---
 module "bastion" {
-  source           = "../../modules/bastion"
+  source           = "../modules/bastion"
   name_prefix      = var.name_prefix
   vpc_id           = module.vpc.vpc_id
   subnet_id        = module.vpc.public_subnet_ids[0]
@@ -27,13 +23,13 @@ module "bastion" {
 
 # --- IAM Module ---
 module "iam" {
-  source       = "../../modules/iam"
+  source       = "../modules/iam"
   cluster_name = var.cluster_name
 }
 
 # --- EKS Module ---
 module "eks" {
-  source               = "../../modules/eks"
+  source               = "../modules/eks"
   cluster_name         = var.cluster_name
   eks_version          = var.eks_version
   eks_cluster_role_arn = module.iam.eks_cluster_role_arn
--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "purely-webapp-tfstate-dev-us-east-1"
-    key            = "dev/terraform.tfstate"
+    key            = "prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "purely-webapp-tfstate-lock-us-east-1"
     encrypt        = true
